da62e3292fb3c177059974342ee1a5354c9faa06
ducnam0902 <ducnam0902@gmail.com>
2021-08-12 23:31:10 +0700

    finish-navbar

--- src/components/Header/components/CartHeader.tsx
+++ src/components/Header/components/CartHeader.tsx
@@ -1,38 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { featureProduct } from '../../../constants/images'
 const CartHeader = () => {
+    const renderCartList = () => {
+        return (
+            featureProduct.map((values, key) => (
+                <div key={key} className="cart-item">
+                    <div className="cart-item_image">
+                        <Link className="cart-item_image_link" to="/">
+                            <img className="img-fluid cart-item-image-img" src={values.image} alt={values.title}></img>
+                        </Link>
+                    </div>
+                    <div className="cart-item_info">
+                        <Link to="/" className="cart-item_title">{values.title}</Link>
+                        <p className="cart-item_quantity">Qty: {10}</p>
+                        <p className="cart-item_price">{values.oldPrice}</p>
+                    </div>
+                    <div className="cart-item_remove">
+                    <i className="ti ti-close cart-item_remove-icon" aria-hidden="true"></i>
+                    </div>
+                </div>
+            ))
+
+        )
+    }
     return (
         <div className="header-cart">
             <span>0</span>
             <i className="ti ti-shopping-cart"></i>
             <div className="header-cart-nav">
                 <div className="cart-list">
-
-                    <div className="cart-item">
-
-                        <div className="cart-item_image">
-                        <Link className="cart-item_image_link" to="/"></Link>
-                        </div>
-
-                        <div className="cart-item_info">
-                            <Link to="/" className="cart-item_title"></Link>
-                            <p className="cart-item_quantity"></p>
-                            <p className="cart-item_price"></p>
-                        </div>
-
+                    {renderCartList()}
+                </div>
+                <div className="header-cart_summary">
+                    <div className="header-cart_total-item">
+                        <p className="header-cart_total">Total</p>
+                        <p className="header-cart_price">$35</p>
                     </div>
-                    <div className="header-cart_summary">
-                        <div className="header-cart_total">
-                            <p className="header-cart_total">Total</p>
-                            <p className="header-cart_price"></p>
-                        </div>
-                        <div className="hearder-cart_function">
-                            <div className="header-cart_viewcart">View Cart</div>
-                            <div className="header-cart_checkout">Checkout</div>
-                        </div>
+                    <div className="header-cart_function">
+                        <div className="header-cart_viewcart">View Cart</div>
+                        <div className="header-cart_checkout">Checkout</div>
                     </div>
                 </div>
+
             </div >
         </div >
     )
