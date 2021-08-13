71bbcfecc44f340cedebbdcf25e831e67c4c7794
ducnam0902 <ducnam0902@gmail.com>
2021-08-13 14:53:30 +0700

    update-nav-bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
 import CartHeader from './components/CartHeader'
 import Currency from './components/Currency'
+
 const Header = () => {
+    const [openNav, setOpenNav] = useState(false);
     return (
         <header>
             <div className="container">
@@ -43,9 +45,49 @@ const Header = () => {
                                     <Link to="/" className="main-nav_item--link">Contact</Link>
                                 </li>
                             </ul>
-                            <Currency/>
-                            <CartHeader/>
+                            <Currency />
+                            <CartHeader />
+                            <div className="header-mobile" onClick={() => setOpenNav(!openNav)}>
+                                {openNav ? <i className=" ti ti-close header-mobile-icon" /> : <i className=" ti ti-menu header-mobile-icon" />}
+                            </div>
                         </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12">
+                        <ul className="mobile-nav">
+                            <li className="mobile-nav_item">
+                                <Link to="/" className="mobile-nav_item-link">Home</Link>
+                            </li>
+                            <li className="mobile-nav_item">
+                                <Link to="/" className="mobile-nav_item-link">About</Link>
+                            </li>
+                            <li className="mobile-nav_item">
+                                <div className="mobile-nav_group" >
+                                    <Link to="/" className="mobile-nav_item-link">Categories</Link>
+                                    <i className="ti ti-plus mobile-nav_item-icon" aria-hidden="true"></i>
+                                    {/* <i className="ti ti-minus mobile-nav_item-icon" aria-hidden="true"></i> */}
+                                </div>
+                                <ul className="mobile-sub-nav">
+                                    <li className="mobile-sub-nav_item">
+                                        <Link to="/" className="mobile-sub-nav_item-link">Black Tea</Link>
+                                    </li>
+                                    <li className="mobile-sub-nav_item">
+                                        <Link to="/" className="mobile-sub-nav_item-link">Mix Tea</Link>
+                                    </li>
+                                    <li className="mobile-sub-nav_item">
+                                        <Link to="/" className="mobile-sub-nav_item-link">Love Tea</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="mobile-nav_item">
+                                <Link to="/" className="mobile-nav_item-link">Blog</Link>
+
+                            </li>
+                            <li className="mobile-nav_item">
+                                <Link to="/" className="mobile-nav_item-link">Contact</Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
---
+++ src/components/Header/components/HeaderMobile.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderMobile = () => {
+    return (
+        <div className="header-mobile">
+            <i className=" ti ti-menu header-mobile-icon"/>
+            {/* <i className=" ti ti-close header-mobile-icon"/> */}
+
+           
+        </div>
+    )
+}
+
+export default HeaderMobile
+
