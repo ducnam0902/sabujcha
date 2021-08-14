ae36da98ab50f3f84e23de75e29b66e8e14a9565
ducnam0902 <ducnam0902@gmail.com>
2021-08-14 22:50:32 +0700

    update-search-store

--- src/components/FilterCategory/FilterCategory.tsx
+++ src/components/FilterCategory/FilterCategory.tsx
@@ -3,32 +3,31 @@ import { collectionItem } from '../../constants/images';
 import FilterList from './components/FilterList';
 import { vendors } from '../../constants/vendors';
 import { sizes } from '../../constants/sizes';
-import { colors } from '../../constants/color'; 
+import { colors } from '../../constants/color';
+import { Link } from 'react-router-dom';
 const FilterCategory = () => {
 
     return (
         <div className="filter-category">
-            <div className="search-store">
-                <form action="">
-                    <input type="text" className="search-store-input" placeholder="Search our store" />
-                    <button className="ti ti-search search-store-button">
-                    </button>
-                </form>
-            </div>
+            <form action="" className="search-store">
+                <input type="text" className="search-store-input" placeholder="Search our store" />
+                <button className="ti ti-search search-store-button">
+                </button>
+            </form>
             <div className="filter-wrapper">
-                <div className="filter-category">
+                <div className="filter-category-group">
                     <h4 className="filter-category-title">Categories</h4>
                     <ul className="filter-category-list">
                         {collectionItem.map((values, key) => (
                             <li key={key} className="filter-category-item">
-                                {values.title}
+                                <Link className="filter-category-link" to={`/categories?name=${values.title}`}>{values.title}</Link>
                             </li>
                         ))}
                     </ul>
                 </div>
-                <FilterList title="Vendors" data={vendors}/>
-                <FilterList title="Color" data={colors}/>
-                <FilterList title="Size" data={sizes}/>
+                <FilterList title="Vendors" data={vendors} />
+                <FilterList title="Color" data={colors} />
+                <FilterList title="Size" data={sizes} />
 
             </div>
         </div>
